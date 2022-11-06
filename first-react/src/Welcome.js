ffe8d73a506db4bd0327e60b2dc890f2ce3902ad
carami <carami@nate.com>
2022-11-06 21:42:22 +0900

    first commit

--- first-react/src/Welcome.js
+++
@@ -1,5 +0,0 @@
-function Welcome(props) {
-  return props.flag ? <h1>안녕하세요</h1> : null;
-}
-
-export default Welcome;
--- proj01/JAVASCRIPT/06.js
+++
@@ -1,9 +0,0 @@
-function work() {
-  const start = Date.now();
-  for (let i = 0; i < 1000000000; i++) {}
-  const end = Date.now();
-  console.log(end - start + "ms");
-}
-console.log("작업 전");
-work();
-console.log("다음 작업");
