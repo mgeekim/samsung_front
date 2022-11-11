a3f1b0a212b879a62c95c7f6a7f9f69f00ebf4bd
carami <carami@nate.com>
2022-11-11 17:07:05 +0900

    final code

--- todo-react/src/ToDoBox.js
+++ todo-react/src/ToDoBox.js
@@ -1,26 +1,74 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
 import ToDoInput from "./ToDoInput";
 import ToDoList from "./ToDoList";
 
 const ToDoBox = () => {
-  const [toDoList, setToDoList] = useState([
-    { id: 1, title: "할일 111" },
-    { id: 2, title: "할일 2222" },
-    { id: 3, title: "할일 3333" },
-  ]);
-  const nextId = useRef(4);
+  const [toDoList, setToDoList] = useState([]);
+
+  useEffect(() => {
+    async function getTodos() {
+      const result = await axios.get(`http://localhost:3030/api/todos`);
+      setToDoList(result.data);
+    }
+
+    getTodos();
+  }, []);
+
+  //   const nextId = useRef(4);
   const addToDoHandler = (toDo) => {
     console.log("todobox ::" + toDo);
     const newTodo = {
-      id: nextId.current++,
       title: toDo,
     };
-    setToDoList([...toDoList, newTodo]);
+
+    async function addToDo(newTodo) {
+      await axios
+        .post(`http://localhost:3030/api/todos`, newTodo)
+        .then(function (response) {});
+
+      const result = await axios.get(`http://localhost:3030/api/todos`);
+      setToDoList(result.data);
+    }
+
+    addToDo(newTodo);
+  };
+  const deleteHandler = (id) => {
+    // setToDoList(toDoList.filter((toDo) => toDo.id !== id));
+    async function deleteTodo(id) {
+      await axios.delete(`http://localhost:3030/api/todos/${id}`);
+
+      const result = await axios.get(`http://localhost:3030/api/todos`);
+      setToDoList(result.data);
+    }
+
+    deleteTodo(id);
+  };
+
+  const updateToDo = (toDo) => {
+    // setToDoList(
+    //   toDoList.map((item) => {
+    //     if (item.id === toDo.id) {
+    //       item.title = toDo.title;
+    //     }
+    //   })
+    // );
+    async function updateToDo(todo) {
+      await axios.patch(`http://localhost:3030/api/todos`, todo);
+
+      const result = await axios.get(`http://localhost:3030/api/todos`);
+      setToDoList(result.data);
+    }
+    updateToDo(toDo);
   };
   return (
     <div>
       <ToDoInput addToDoHandler={addToDoHandler} />
-      <ToDoList toDoList={toDoList} />
+      <ToDoList
+        toDoList={toDoList}
+        deleteHandler={deleteHandler}
+        updateToDo={updateToDo}
+      />
     </div>
   );
 };
--- todo-react/src/ToDoList.js
+++ todo-react/src/ToDoList.js
@@ -1,8 +1,18 @@
-const ToDoList = ({ toDoList }) => {
+import ToDo from "./ToDo";
+
+const ToDoList = ({ toDoList, deleteHandler, updateToDo }) => {
   return (
     <ul>
       {toDoList.map((toDo) => (
-        <li key={toDo.id}>{toDo.title}</li>
+        <li key={toDo.id}>
+          {/* {toDo.title}{" "}
+          <button onClick={() => deleteHandler(toDo.id)}>삭제</button> */}
+          <ToDo
+            toDo={toDo}
+            deleteHandler={deleteHandler}
+            updateToDo={updateToDo}
+          />
+        </li>
       ))}
     </ul>
   );
