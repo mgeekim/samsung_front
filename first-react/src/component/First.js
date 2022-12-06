a9a3d64a718b8f3bd15221b85faa489bf3a9c308
carami <carami@nate.com>
2022-12-06 11:50:11 +0900

    html,css 예제

--- first-react/src/component/First.js
+++
@@ -1,5 +0,0 @@
-const First = ({ name }) => {
-  return <h3>First child {name}</h3>;
-};
-
-export default First;
--- first-react/src/component/Second.js
+++
@@ -1,5 +0,0 @@
-const Second = () => {
-  return <h3>second child!!</h3>;
-};
-
-export default Second;
