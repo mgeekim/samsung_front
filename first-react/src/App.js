f6e79803655c0fec66cb8a0858ff122a1de191f1
carami <carami@nate.com>
2022-12-08 16:40:24 +0900

    add exam

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,7 +1,9 @@
+import MyInputBox from "./compont/MyInputBox";
 import ChildrenExam from "./compont/propsChildren/ChildrenExam";
 import UseStateExam1 from "./compont/useState/UseStateExam1";
 import UseStateExam2 from "./compont/useState/UseStateExam2";
 import UseStateExam3 from "./compont/useState/UseStateExam3";
+import Welcome from "./compont/Welcome";
 import Counter from "./Counter";
 import Hello from "./Hello";
 
@@ -13,11 +15,15 @@ function App() {
       {/* <h1>hello {name}!!! </h1>
       <Counter />
 
-      <Hello color="blue" name="carami" />
       <Hello></Hello>
 
       <ChildrenExam /> */}
-      <UseStateExam3 />
+      {/* <UseStateExam3 /> 
+      <Welcome flag={false} />
+
+      <Hello color="blue" name="carami" isSpecial={true} />
+      */}
+      <MyInputBox />
     </div>
   );
 }
---
+++ first-react/src/compont/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = (props) => {
+  return props.flag ? <h1>안녕하세요.^^</h1> : null;
+};
+
+export default Welcome;
