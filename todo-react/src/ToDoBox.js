c021924ce686dc77bc0cbf4366befc6e7572b635
carami <carami@nate.com>
2023-02-24 16:48:42 +0900

    todo update

--- todo-react/src/ToDoBox.js
+++ todo-react/src/ToDoBox.js
@@ -1,39 +1,71 @@
 import ToDoInput from "./ToDoInput";
 import ToDoList from "./ToDoList";
+import axios from "axios";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const ToDoBox = () => {
   console.log("todoBox ✌✌");
-  const [toDoList, setToDoList] = useState([
-    { id: 1, title: "Learn React" },
-    { id: 2, title: "Learn GraphQL" },
-    { id: 3, title: "Learn Apollo" },
-  ]);
+  const [toDoList, setToDoList] = useState([]);
 
-  const nextId = useRef(4);
+  useEffect(() => {
+    async function getTodos() {
+      const result = await axios.get("http://localhost:3333/api/todos");
+      setToDoList(result.data);
+    }
+
+    getTodos();
+  }, []);
+
+  //   const nextId = useRef(4);
   const addToDoHandler = (title) => {
     // console.log(title);
     const toDo = {
-      id: nextId.current,
+      //   id: nextId.current,
       title: title,
     };
 
-    setToDoList([...toDoList, toDo]);
+    async function addTodo(toDo) {
+      await axios
+        .post("http://localhost:3333/api/todos", toDo)
+        .then(function (response) {});
 
-    nextId.current += 1;
+      const result = await axios.get("http://localhost:3333/api/todos");
+      setToDoList(result.data);
+    }
+
+    // setToDoList([...toDoList, toDo]);
+    // nextId.current += 1;
+    addTodo(toDo);
   };
 
   const deleteHandler = (id) => {
-    setToDoList(toDoList.filter((toDo) => toDo.id !== id));
+    // setToDoList(toDoList.filter((toDo) => toDo.id !== id));
+    async function deleteTodo(id) {
+      await axios.delete(`http://localhost:3333/api/todos/${id}`);
+
+      const result = await axios.get("http://localhost:3333/api/todos");
+      setToDoList(result.data);
+    }
+
+    deleteTodo(id);
   };
 
   function updateTodo(toDo) {
-    toDoList.map((item) => {
-      if (item.id === toDo.id) {
-        item.title = toDo.title;
-      }
-    });
+    async function updateTodo(todo) {
+      await axios.patch("http://localhost:3333/api/todos", todo);
+
+      const result = await axios.get("http://localhost:3333/api/todos");
+      setToDoList(result.data);
+    }
+
+    updateTodo(toDo);
+
+    //     toDoList.map((item) => {
+    //       if (item.id === toDo.id) {
+    //         item.title = toDo.title;
+    //       }
+    //     });
   }
 
   return (
